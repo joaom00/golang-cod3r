b4610a67f04cccc8e64eed49c0f71835a051043a
João Pedro <joaomljcanal@gmail.com>
2021-08-09 10:52:56 -0300

    padrão de concorrência generators

---
+++ concorrencia/generator/generator.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joaom00/golang/html"
+)
+
+func main() {
+	t1 := html.Titulo("https://www.google.com", "https://www.cod3r.com.br")
+	t2 := html.Titulo("https://www.amazon.com", "https://www.youtube.com")
+	fmt.Println("Primeiros:", <-t1, "|", <-t2)
+	fmt.Println("Segundos:", <-t1, "|", <-t2)
+}
