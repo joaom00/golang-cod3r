3f37d4eff0253e4ff81ae7f6ff4a779fecc21f4f
João Pedro <joaomljcanal@gmail.com>
2021-09-16 12:07:15 -0300

    sql

---
+++ sql/select/select.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type usuario struct {
+	id   int
+	nome string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:123456@/cursogo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, _ := db.Query("select id, nome from usuarios where id > ?", 3)
+	defer rows.Close()
+
+	for rows.Next() {
+		var u usuario
+		rows.Scan(&u.id, &u.nome)
+		fmt.Println(u)
+	}
+}
